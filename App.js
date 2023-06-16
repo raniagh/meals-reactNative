74047a4383f7c8f39ac87ac3f357a1d9d25d7210
Your Name <rghzaiel@ggn-group.com>
2023-06-16 15:09:07 +0100

    add nested navigators

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: "All Categories" }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -20,9 +42,9 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{ title: "All Categories" }}
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
           ></Stack.Screen>
           <Stack.Screen
             name="MealsOverview"
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+
+function FavoritesScreen() {
+  return <Text>Favorites Screen</Text>;
+}
+export default FavoritesScreen;
