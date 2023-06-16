937f428130e74613ccb0bd76747636c212284568
Your Name <rghzaiel@ggn-group.com>
2023-06-16 08:31:03 +0100

    first commit

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+function CategoriesScreen() {
+  return (
+    <View style={StyleSheet.container}>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+    </View>
+  );
+}
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
