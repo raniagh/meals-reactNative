5b2c7db0aa15f19c891d079a87bcdff13222f0e6
Your Name <rghzaiel@ggn-group.com>
2023-06-17 10:50:45 +0100

    add no favorite meals part

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,12 +2,33 @@ import { useContext } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 import MealsList from "../components/MealsList/MealsList";
+import { StyleSheet, Text, View } from "react-native";
 
 function FavoritesScreen() {
   const favoriteMealsCtx = useContext(FavoritesContext);
   const mealIds = favoriteMealsCtx.ids;
 
   const favoriteMeals = MEALS.filter((meal) => mealIds.includes(meal.id));
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have No favorite meals yet.</Text>
+      </View>
+    );
+  }
   return <MealsList meals={favoriteMeals} />;
 }
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
